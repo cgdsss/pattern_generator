fd2c17cf34eeadb89d8d72461788180b30e985bb
cgd_Lab16 <cgdsss@mail.ustc.edu.cn>
2018-08-18 16:31:42 +0800

    convenient to use detection

--- tag.py
+++ tag.py
@@ -12,11 +12,19 @@ tags_id = cfg['tags_id']
 tags_size = cfg['tags_size']
 pdf_size = cfg['pdf_size']
 
-pdf = FPDF(orientation = 'P', unit = 'mm', format=pdf_size)
+pdf = FPDF(orientation = 'P', unit = 'cm', format=(pdf_size[0]*100.0, pdf_size[1]*100.0))
 pdf.add_page()
+print "Drawing: "
 for i in range(len(tags_id)):
+    print "--"
+    print "id: ", str(tags_id[i])
+    print "size: {0}m".format(tags_size[i])
+    print "position: {0}m, {1}m".format(tags_pose[i][0], tags_pose[i][1])
     img = os.path.join(current_path, 'png/' + tag_type + "/" + str(tags_id[i]) + ".png")
-    pdf.image(img, (tags_pose[i][0] - tags_size[i] / 2.0) + pdf_size[0]/2.0, (tags_pose[i][1] - tags_size[i] / 2.0) + pdf_size[1]/2.0,
-        tags_size[i], tags_size[i])
+    pdf.image(img, ((tags_pose[i][0] - tags_size[i] / 2.0) + pdf_size[0]/2.0) * 100.0,
+        ((-tags_pose[i][1] - tags_size[i] / 2.0) + pdf_size[1]/2.0) * 100.0,
+        tags_size[i] * 100.0, tags_size[i] * 100.0)
 
 pdf.output(os.path.join(current_path, 'pattern.pdf'), 'F')
+print "Already generated: pattern.pdf"
+print "dir:", current_path
